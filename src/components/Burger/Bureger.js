39e78afb2f3ebd9a1807b74425c4da548814db46
iskanderAB <iskanderabasi@gmail.com>
2020-05-08 03:06:46 +0100

    burgerBuilder

--- src/components/Burger/Bureger.js
+++ src/components/Burger/Bureger.js
@@ -1,1 +1,15 @@
- 
+import React from "react";
+import classes from "./Bureger.css"
+import BurgerIngredient from "./BurgerIngridient/BurgerIngredient";
+const Burger = ()=> {
+    return(
+     <div className={classes.Burger}>
+         <BurgerIngredient type="bread-top"/>
+         <BurgerIngredient type="cheese"/>
+         <BurgerIngredient type="meat"/>
+         <BurgerIngredient type="bread-bottom"/>
+     </div>
+    );
+};
+export default Burger;
+
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, {Component} from "react";
 import MyAux from "../../hoc/MyAux";
+import Burger from "../../components/Burger/Bureger";
 class BurgerBuilder extends Component {
+    state = {
+        ingredients:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    };
     render() {
         return(
             <MyAux>
                 <div>
-                    Burger
+                   <Burger ingredients={this.state.ingredients } />
                 </div>
                 <div>
                     BurgerControls
